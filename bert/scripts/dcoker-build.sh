0be1e421567bfdf5f768ad15c60a1e5049ce3c45
perambluate <dx48231@gmail.com>
2020-06-03 20:56:48 +0800

    change the configuration of dir-tree and add docker file based on ngc image for bert

---
+++ bert/scripts/dcoker-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DKF=Dockerfile
+CTN=bert
+BASE="nvcr.io/nvidia/tensorflow:20.03-tf1-py3"
+
+if [ $# != 0 ]; then
+    echo "Please input BASE DKF CTN in order"
+    DKF=$1
+    CTN=${2:-$CTN}
+    BASE=${3:-$BASE}
+fi
+
+docker pull $BASE
+docker build -f $DKF --rm -t $CTN --build-arg BASE=$BASE .
---
+++ bert/scripts/openmpi_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+VER=3.1.5
+MPIDIR=/opt/mpi
+DOWNLOAD=/tmp
+URL=https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-${VER}.tar.gz
+
+if [ $# != 0 ];then
+    if [[ $1 != ^(3,4)\.* ]]; then
+        echo "Unsurported version of Open MPI!!!"
+        exit 1;
+    elif [[ $1 != ^3\.* ]]; then
+        VER=$1
+        URL=https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-${VER}.tar.gz
+    else
+        URL=https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-${VER}.tar.gz
+    fi
+    if [[ -n $2 ]]; then
+        $MPIDIR=$2
+    fi
+fi
+
+wget -P $DOWNLOAD $URL && \
+tar -zxvf $DOWNLOAD/openmpi-${VER}.tar.gz -C $MPIDIR && \
+rm $DOWNLOAD/openmpi-${VER}.tar.gz && \
+mkdir -p $MPIDIR/openmpi && \
+cd $MPIDIR/openmpi-${VER} && \
+CC=gcc FC=gfortran ./configure --with-cuda --prefix=$MPIDIR/openmpi/$VER && \
+make -j$(nproc) && make install -j$(nproc)
