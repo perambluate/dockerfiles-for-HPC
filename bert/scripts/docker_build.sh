4e960193e9290a0b2d60677094f62cac1874ecee
perambluate <dx48231@gmail.com>
2020-06-07 14:43:02 +0800

    debug during building time

--- bert/scripts/docker_build.sh
+++ bert/scripts/docker_build.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-fn=Dockerfile
+df=bert/Dockerfile
 tag=bert-test
 base="nvcr.io/nvidia/tensorflow:20.03-tf1-py3"
 
  while [[ -n $@ ]]; do
         temp=$1
         case ${temp%%=*} in
-            "fn")
-                fn=${temp#fn=};;
+            "df")
+                df=${df:-${temp#df=}};;
             "tag")
-                tag=${temp#tag=};;
+                tag=${tag:-${temp#tag=}};;
             "base")
-                base=${temp#base=};;
+                base=${base:-${temp#base=}};;
             *)
-                echo "Wrong input, please check this shell script"
+                echo "Wrong input, please input as following"
+		echo "df=<path_to_Dockerfile> tag=<tag_for_docker_image> base=<the_base_image_for_building>"
                 return 1;;
         esac
         shift
     done
 
 docker pull $base
-docker build --file bert/$fn --rm -t $tag --build-arg BASE=$base .
+docker build --file $df --rm -t $tag --build-arg BASE=$base .
