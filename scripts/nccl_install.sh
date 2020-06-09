06416ff30246cd30db04aa60ba833886f2adf10d
perambluate <dx48231@gmail.com>
2020-06-09 21:29:22 +0800

    specify dir for nccl installation in nccl_install.sh

--- scripts/nccl_install.sh
+++ scripts/nccl_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+NCCL_DIR=/opt/nccl
 #prerequisite
 apt update && apt autoremove libnccl* &&\
 apt install -y build-essential devscripts debhelper fakeroot git
 
 #install from nccl github
-git clone https://github.com/NVIDIA/nccl.git &&\
-cd nccl && make pkg.debian.build -j$(nproc) && dpkg -i build/pkg/deb/libnccl*
+git clone https://github.com/NVIDIA/nccl.git ${NCCL_DIR} &&\
+cd ${NCCL_DIR} && \
+make pkg.debian.build -j$(nproc) && \
+dpkg -i build/pkg/deb/libnccl*
