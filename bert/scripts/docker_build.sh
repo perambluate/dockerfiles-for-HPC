4717ba34ebf88099f66ef96317a3f1e06410dd3c
perambluate <dx48231@gmail.com>
2020-06-09 21:08:40 +0800

    modify the path in bert related modulfiles, add a sciprt for running and tuning run_classifier.py and modify bert Dockerfile & build script

--- bert/scripts/docker_build.sh
+++ bert/scripts/docker_build.sh
@@ -5,24 +5,27 @@ tag=bert-test
 ompi="nvidia/cuda:10.2-devel-ubuntu16.04"
 bert="nvcr.io/nvidia/tensorflow:20.03-tf1-py3"
 
- while [[ -n $@ ]]; do
-        temp=$1
-        case ${temp%%=*} in
-            "df")
-                df=${df:-${temp#df=}};;
-            "tag")
-                tag=${tag:-${temp#tag=}};;
-            "bert")
-                bert=${base:-${temp#bert=}};;
+while [[ -n $@ ]]; do
+    temp=$1
+    case ${temp%%=*} in
+        "df")
+            df=${df:-${temp#df=}};;
+        "tag")
+            tag=${tag:-${temp#tag=}};;
+        "bert")
+            bert=${bert:-${temp#bert=}};;
 	    "ompi")
-		ompi=${ompi:-${temp#ompi=}};;
-            *)
-                echo "Wrong input, please input as following"
-		echo "df=<path_to_Dockerfile> tag=<tag_for_docker_image> bert=<the_base_image_for_bert_container> ompi=<the_base_image_for_openmpi_installation>"
-                return 1;;
-        esac
-        shift
-    done
+		    ompi=${ompi:-${temp#ompi=}};;
+        *)
+            echo "Wrong input, please input as following"
+		    echo "df=<path_to_Dockerfile> \
+                tag=<tag_for_docker_image> \
+                bert=<the_base_image_for_bert_container> \
+                ompi=<the_base_image_for_openmpi_installation>"
+            exit 1;;
+    esac
+    shift
+done
 
 docker pull $bert && docker pull $ompi && \
 docker build --file $df --rm -t $tag --build-arg BERT_BASE=$bert --build-arg OMPI_BASE=$ompi .
---
+++ bert/scripts/run_classifier.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+TASK=MNLI
+SOURCE=nvidia
+MODEL=uncased
+MPI=open
+NP=1
+LOG=${TASK}_${SOURCE}_${MODEL}_${MPI}
+ERR=0
+
+ReadInput(){
+	while [[ -n $@ ]]; do
+    	temp=$1
+    	case ${temp%%=*} in
+        	"code")
+            	SOURCE=${SOURCE:-${temp#code=}};;
+        	"model")
+	            MODEL=${MODEL:-${temp#model=}};;
+			"outdir")
+				OUTDIR=${OUTDIR:-${temp#outdir=}};;
+	    	"mpi")
+				MPI=${MPI:-${temp#mpi=}};;
+			"np")
+				NP=${NP:-${temp#np=}};;
+			"mpi_arg")
+				MPI_ARG=${MPI_ARG:-${temp#mpi_arg=}};;
+        	*)
+            	echo "Wrong input, please input as following"
+				echo "code=<using_nvidia_or_google_github> \
+					model=<the_pretrained_model_for_fine_tuning> \
+					mpi=<the_version_of_mpi_to_run> \
+					mpi_arg=<the_arguments_for_mpirun>"
+	            exit 1;;
+    	esac
+	    shift
+	done
+}
+
+ModuleLoad(){
+	MPI_MODULE=mpi/${MPI}
+	[[ ${MODEL%%_*} == 'wwm' ]] && BERT_MODULE=bert/${SOURCE}/large/wwm/${MODEL#*_}/${TASK} || BERT_MODULE=bert/${SOURCE}/large/${MODEL#*_}/${TASK}
+	module purge && \
+	module load ${MPI_MODULE} && \
+	module load ${BERT_MODULE}
+	ErrHandle $? 'ModuleLoad'
+}
+
+SetMPIExec(){
+	if (( NP > 1 )); then
+		[[ ${MPI} == 'open' ]] && ROOT_ALLOW='--allow-run-as-root' || ROOT_ALLOW=
+		MPIEXEC='mpirun'${ROOT_ALLOW}'-np '${NP}' '${MPI_ARG}
+	else
+		MPIEXEC=
+	fi
+}
+
+ErrHandle(){
+	ERR=${ERR:-$1}
+	BLOCK=$2
+	if [[ $1 != 0 ]]; then
+        printf "Some errors occurred in %s, please fix it and modify the script!!\n" ${BLOCK}
+        exit 1;
+    fi
+}
+
+ReadInput $@
+ModuleLoad
+SetMPIExec
+OUTDIR=results/${LOG}
+[ -d ${OUTDIR} ] || mkdir -p ${OUTDIR}
+time -p \
+${MPIEXEC} \
+python ${CODE_PATH}/run_classifier.py \
+--do_train=true \
+--do_eval=false \
+--do_predict=false \
+--do_lower_case=false \
+--task_name=${TASK} \
+--data_dir=${DATA_PATH} \
+--vocab_file=${MODEL_PATH}/vocab.txt \
+--bert_config_file=${MODEL_PATH}/bert_config.json \
+--init_checkpoint=${MODEL_PATH}/bert_model.ckpt \
+--output_dir=${OUTDIR} \
+--learning_rate=5e-5 \
+--num_train_epochs=0.001 \
+--max_seq_length=128 \
+--train_batch_size=1 \
+--num_accumulation_steps=1 \
+--save_checkpoints_steps=1000 \
+--warmup_proportion=0.1 \
+--use_fp16 \
+--horovod \
+2>&1 | tee ${LOG}.log
